996800383bc95b1eeafddb99e2bf4469a5430fe5
Lavan Nithi <lavannithi2@gmail.com>
2024-04-12 22:12:38 -0400

    Front-end pulls from the API

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_cors import CORS
+import json, time
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    data_set = {'Page': 'Home', 'Message': 'Successfully loaded the Home page', 'Timestamp': time.time()}
+    json_dump = json.dumps(data_set)
+    return json_dump
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=4000)
